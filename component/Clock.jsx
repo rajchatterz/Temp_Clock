ac7f56d1902033703abc85f8d4a5035a71037d08
rajchatterz <iamraj.space@gmail.com>
2023-12-11 16:13:21 +0530

    done with the analog clock design

--- component/Clock.jsx
+++ component/Clock.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { View, Text } from "react-native";
-import { Svg, Line, Circle, Rect } from "react-native-svg";
+import { View } from "react-native";
+import { Svg, Line, Circle, Rect, Text } from "react-native-svg";
 
 const Clock = () => {
   const [time, setTime] = useState(new Date());
@@ -25,6 +25,47 @@ const Clock = () => {
           stroke="black"
           strokeWidth={2}
           fill={"white"}
+          style={{ elevation: 100 }}
+        />
+        <Line
+          x1={100}
+          y1={100}
+          x2={100}
+          y2={35}
+          strokeWidth={6}
+          stroke={"black"}
+          rotation={hour * 30 + minute * 0.5}
+          origin={(100, 100)}
+        />
+        <Line
+          x1={100}
+          y1={100}
+          x2={100}
+          y2={70}
+          strokeWidth={6}
+          stroke={"black"}
+          rotation={hour * 30 + minute * 0.5}
+          origin={(100, 100)}
+        />
+        <Line
+          x1={100}
+          y1={100}
+          x2={100}
+          y2={50}
+          strokeWidth={4}
+          stroke={"black"}
+          rotation={(minute + second / 60) * 6}
+          origin={(100, 100)}
+        />
+        <Line
+          x1={100}
+          y1={100}
+          x2={100}
+          y2={20}
+          strokeWidth={3}
+          stroke={"black"}
+          rotation={second * 6}
+          origin={(100, 100)}
         />
       </Svg>
     </View>
