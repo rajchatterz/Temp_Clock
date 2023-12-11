82c2521df7f51ed4c8c92d7ab879a935756fbeb1
rajchatterz <iamraj.space@gmail.com>
2023-12-11 19:15:40 +0530

    done with the analog clock stying

--- component/Clock.jsx
+++ component/Clock.jsx
@@ -1,72 +1,82 @@
 import React, { useEffect, useState } from "react";
 import { View } from "react-native";
-import { Svg, Line, Circle, Rect, Text } from "react-native-svg";
+import { Svg, Line, Circle, Text } from "react-native-svg";
 
 const Clock = () => {
   const [time, setTime] = useState(new Date());
+
   useEffect(() => {
     const timeId = setInterval(() => {
       setTime(new Date());
-    }, 100);
+    }, 1000);
+
     return () => clearInterval(timeId);
-  });
+  }, []);
+
   const hour = time.getHours();
   const minute = time.getMinutes();
   const second = time.getSeconds();
 
   return (
-    <View>
-      <Text>{time.toLocaleDateString()}</Text>
-      <Svg height={200} width={200}>
+    <View style={{ borderWidth: 1, borderColor: "black" }}>
+      <Svg height={360} width={360}>
         <Circle
-          cx={100}
-          cy={100}
-          r={90}
+          cx={180}
+          cy={180}
+          r={170}
           stroke="black"
-          strokeWidth={2}
+          strokeWidth={10}
           fill={"white"}
-          style={{ elevation: 100 }}
         />
+        {/* Hour hand */}
         <Line
-          x1={100}
-          y1={100}
-          x2={100}
-          y2={35}
-          strokeWidth={6}
+          x1={180}
+          y1={180}
+          x2={180}
+          y2={90}
+          strokeWidth={8}
+          strokeLinecap="round"
           stroke={"black"}
           rotation={hour * 30 + minute * 0.5}
-          origin={(100, 100)}
+          origin={"180, 180"}
         />
+        {/* Minute hand */}
         <Line
-          x1={100}
-          y1={100}
-          x2={100}
-          y2={70}
+          x1={180}
+          y1={180}
+          x2={180}
+          y2={60}
           strokeWidth={6}
-          stroke={"black"}
-          rotation={hour * 30 + minute * 0.5}
-          origin={(100, 100)}
-        />
-        <Line
-          x1={100}
-          y1={100}
-          x2={100}
-          y2={50}
-          strokeWidth={4}
+          strokeLinecap="round"
           stroke={"black"}
           rotation={(minute + second / 60) * 6}
-          origin={(100, 100)}
+          origin={"180, 180"}
         />
+        {/* Second hand */}
         <Line
-          x1={100}
-          y1={100}
-          x2={100}
-          y2={20}
-          strokeWidth={3}
-          stroke={"black"}
+          x1={180}
+          y1={180}
+          x2={180}
+          y2={50}
+          strokeWidth={4}
+          stroke={"red"}
+          strokeLinecap="round"
           rotation={second * 6}
-          origin={(100, 100)}
+          origin={"180, 180"}
         />
+
+        <Circle cx={180} cy={180} r={7} fill={"black"} />
+
+        <Text
+          x={190}
+          y={25}
+          fontSize={20}
+          fill={"black"}
+          fontWeight={"bold"}
+          textAnchor="middle"
+        >
+          12
+        </Text>
       </Svg>
     </View>
   );
